b16ea0dba5493dc39eced0ea800d07c0a617ea65
ana diaz <serielain@gmail.com>
2021-06-15 18:03:19 -0400

    se crea validación de producto de prueba con metodo get producto1

--- demo/src/main/java/com/musicprofin/demo/rest/ProductosRest.java
+++ demo/src/main/java/com/musicprofin/demo/rest/ProductosRest.java
@@ -1,16 +1,27 @@
 package com.musicprofin.demo.rest;
 
-//import org.springframework.web.bind.annotation.GetMapping;
+import com.musicprofin.demo.entitys.productos;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class ProductosRest {
+
+    @GetMapping
+    public ResponseEntity<productos> getProductos(){
+        productos producto = new productos();
+        producto.setId(1L);
+        producto.setName("Producto1");
+        return ResponseEntity.ok(producto);
+    }
     
     //@GetMapping//localhost:7888
-    @RequestMapping(value="hello", method = RequestMethod.GET)
+    //@RequestMapping(value="hello", method = RequestMethod.GET)
     public String hello(){
         return "Hello world";
     }
