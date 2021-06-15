dd4b71e67c9adab203e7681d34ff98af933e6d2a
ana diaz <serielain@gmail.com>
2021-06-15 15:04:03 -0400

    prueba @GetMapping

---
+++ demo/src/main/java/com/musicprofin/demo/rest/ProductosRest.java
@@ -0,0 +1,17 @@
+package com.musicprofin.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class ProductosRest {
+    
+    @GetMapping
+    //@RequestMapping(value="hello", method = RequestMethod.GET)
+    public String hello(){
+        return "Hello world";
+    }
+}
