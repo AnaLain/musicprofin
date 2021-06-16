d97eabb14c219a7ef63e00e2e5c98481e0d6b7d4
ana diaz <serielain@gmail.com>
2021-06-15 20:06:21 -0400

    conexión con BD mysql

--- demo/src/main/java/com/musicprofin/demo/entitys/productos.java
+++ demo/src/main/java/com/musicprofin/demo/entitys/productos.java
@@ -1,12 +1,30 @@
 package com.musicprofin.demo.entitys;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
 public class productos {
     
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name="name", nullable=false, length=30)
     private String name;
+    @Column(name ="Categoria", nullable=false, length=30)
     private String Categoria;
+    @Column(name="cantidad", nullable=false, length=30)
     private int cantidad;
+    @Column(name="valor", nullable=false, length=30)
     private int valor;
+
     public Long getId() {
         return id;
     }
